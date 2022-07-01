38da42c66b5ab839292b7646fb073840587e9477
YangYi <iyoung2018@126.com>
2022-07-01 16:36:52 +0800

    v1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,36 @@
 module.exports={
+  base:'/blog/',
   title:'加班不秃，熬夜会秃',
   description:'博客；前端学习；生活记录',
   // head:[
   //   ['link',{rel:'icon', href:'./'}]
   // ]
+  themeConfig:{
+    nav:[
+      {text:'首页', link:'/'},
+      {text:'GitHub', link:'https://github.com/MatRunner/blog'}
+    ],
+    sidebar:[
+      {
+        title:'welcome',
+        path:'/',
+        collapsable:false,
+        children:[
+          {
+            title:'欢迎页',
+            path:'/'
+          }
+        ]
+      },
+      {
+        title:'page1',
+        path:'/handbook/1',
+        collapsable:false,
+        children:[
+          {title:'1.md',path:'/handbook/1'},
+          {title:'2.md',path:'/handbook/2'}
+        ]
+      }
+    ]
+  },
 }
