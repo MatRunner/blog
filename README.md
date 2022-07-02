066628deead1c1f5c73517510e3fa9753b9c7883
yangyi <iyoung2018@126.com>
2022-07-02 23:56:42 +0800

    7.2

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction
+基于vuepress搭建的极简个人博客，风格暂时受限于个人技术水平，后续博客（可能）会随本人技术提高得到优化。
+搭建参考:point_right:[一篇带你用 VuePress + Github Pages 搭建博客](https://github.com/mqyqingfeng/Blog/issues/235)
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,34 +1,42 @@
-module.exports={
-  base:'/blog/',
-  title:'加班不秃，熬夜会秃',
-  description:'博客；前端学习；生活记录',
+module.exports = {
+  base: '/blog/',
+  title: '加班不秃，熬夜会秃',
+  description: '博客；前端学习；生活记录',
   // head:[
   //   ['link',{rel:'icon', href:'./'}]
   // ]
-  themeConfig:{
-    nav:[
-      {text:'首页', link:'/'},
-      {text:'GitHub', link:'https://github.com/MatRunner/blog'}
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/MatRunner/blog' }
     ],
-    sidebar:[
+    sidebar: [
       {
-        title:'welcome',
-        path:'/',
-        collapsable:false,
-        children:[
+        title: '介绍',
+        path: '/',
+        collapsable: false,
+        sidebarDepth: 0,
+        children: [
           {
-            title:'欢迎页',
-            path:'/'
+            title: '关于本博客',
+            path: '/'
           }
         ]
       },
       {
-        title:'page1',
-        path:'/handbook/1',
-        collapsable:false,
-        children:[
-          {title:'1.md',path:'/handbook/1'},
-          {title:'2.md',path:'/handbook/2'}
+        title: '踩坑合集',
+        path: '/tech/introduction',
+        collapsable: false,
+        children: [
+          { title: '如何发布一个npm包', path: '/tech/npm' },
+        ]
+      },
+      {
+        title: '摸鱼杂谈',
+        path: '/fish/introduction',
+        collapsable: false,
+        children: [
+          { title: '材料那几年', path: '/fish/材料那几年' }
         ]
       }
     ]
